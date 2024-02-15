983a637be0be0cb9a6aaefb870ffe9f6cb9b1d71
hassenmrakbenjebahi <123837615+hassenmrakbenjebahi@users.noreply.github.com>
2024-02-15 22:35:54 +0100

    config

--- models/user.py
+++ models/user.py
@@ -1,16 +1,16 @@
 from flask import Flask
-from flask_pymongo import PyMongo
+#from flask_pymongo import PyMongo
 
-app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mydatabase"
-mongo = PyMongo(app)
+#app = Flask(__name__)
+#app.config["MONGO_URI"] = "mongodb://localhost:27017/mydatabase"
+#mongo = PyMongo(app)
 
 class User:
     def __init__(self, name, age, address):
         self.name = name
         self.age = age
         self.address = address
-
+"""
     def save_to_mongo(self):
         mongo.db.users.insert_one(
             {"name": self.name, "age": self.age, "address": self.address}
@@ -19,3 +19,4 @@ class User:
 # Utilisation de l'exemple ci-dessus
 user = User("Navindu", 22, "Colombo")
 user.save_to_mongo()
+"""
